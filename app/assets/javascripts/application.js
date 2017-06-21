587e27ddfd2e9b1053d88bf0165d89668f6150d5
YangHui <yanghui@tigerjoys.com>
2017-06-21 11:58:58 +0800

    修改提示框

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,48 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function(){
+  window.Notice = {
+    checkAndShow: function () {
+      var $notice = $('#notice')
+      var msg = $notice.text().trim();
+
+      if (msg == ""){
+        $notice.hide();
+        return;
+      }
+      $notice.removeClass();
+      $notice.addClass('alert').addClass('alert-success');
+      this.setPositionAndShow($notice);
+    },
+    show: function(msg, level) {
+      var $notice = $('#notice')
+      $notice.removeClass();
+      $notice.addClass('alert').addClass(level || 'alert-success');
+      $notice.text(msg);
+      this.setPositionAndShow($notice);
+    },
+    setPositionAndShow: function ($notice) {
+      var left = ($(window).width() - $notice.width())/2;
+      $notice.css({top: 100, left: left}).show();
+      setTimeout(function(){
+        $notice.hide();
+      }, 3000)
+    },
+    showError: function(msg) {
+      this.show(msg, 'alert-danger');
+    },
+    showWarn: function(msg) {
+      this.show(msg, 'alert-warning');
+    },
+    showInfo: function(msg) {
+      this.show(msg, 'alert-info');
+    },
+    showSuccess: function(msg) {
+      this.show(msg, 'alert-success');
+    }
+  }
+
+  Notice.checkAndShow();
+});
