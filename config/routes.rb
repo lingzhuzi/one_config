9b3bfc8c7e1e973c524c52b0da1b887cc705b098
YangHui <yanghui@tigerjoys.com>
2017-06-21 15:39:54 +0800

    删除无用的controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :envs
-  get 'home/index'
 
   resources :apps do
     resources :configs
   end
 
-  root 'home#index'
+  root 'apps#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
